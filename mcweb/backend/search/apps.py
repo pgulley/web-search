9825cf65e45e748ed4c841c326134dcf47be3067
Paige Gulley <nano3.14@gmail.com>
2024-08-28 20:07:42 -0400

    raise the providers timeout

--- mcweb/backend/search/apps.py
+++ mcweb/backend/search/apps.py
@@ -5,5 +5,6 @@ import mc_providers
 class SearchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.search'
-    # set a default timeout for all providers
-    mc_providers.set_default_timeout(3*60)
+    # set a default timeout for all providersi
+    # should this be configurable?
+    mc_providers.set_default_timeout(10*60)
